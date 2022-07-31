58e20540afe45ebdf08589002708fed2954b5127
Alexander Latour <alexlatour@gmail.com>
2022-07-31 02:02:20 -0400

    fix

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ def now():
     return round(time.time() * 1000)
 
 command_to_execute = "sudo -u alex rofi -show run"
-time_last_pressed = 0
 last_key_pressed = ""
 
 mod_key = "Key.cmd"
@@ -25,12 +24,9 @@ def set_last_key(key):
 
 def on_press(key):
     set_last_key(key)
-    if is_mod(key):
-        global time_last_pressed
-        time_last_pressed = now()
 
 def on_release(key):
-    if is_mod(key) and now() - time_last_pressed:
+    if is_mod(key):
         global last_key_pressed
         if is_mod(last_key_pressed):
             open_rofi()
