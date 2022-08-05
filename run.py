57c3ad5058426aedb8424e956cade439b9e9ff81
Alexander Latour <alexlatour@gmail.com>
2022-08-05 17:23:18 -0400

    added back max wait time

--- run.py
+++ run.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 import os
+import time
 from pynput import keyboard
+
+def now():
+    return round(time.time() * 1000)
 
 mod_key = "Key.cmd"
 command_to_execute = "rofi -show run"
 
 last_key_pressed = None
-
+time_last_pressed = now()
+max_wait_time = 1000
 
 def is_mod(key):
     return str(key) == mod_key
 
 def on_press(key):
     global last_key_pressed
+    global time_last_pressed
     last_key_pressed = str(key)
+    time_last_pressed = now()
 
 def on_release(key):
     global last_key_pressed
     if is_mod(key) and is_mod(last_key_pressed):
-        os.system(command_to_execute)
-
+        if (now() - time_last_pressed) <= max_wait_time: 
+            os.system(command_to_execute)
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener: listener.join()
-
